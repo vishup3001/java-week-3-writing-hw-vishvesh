0bfcf1607407434e565132959512dfa91120b4d9
vishup3001 <vishveshpatel1511@gmail.com>
2023-03-30 13:44:08 +0100

    my homework week 3

---
+++ src/Prog1OddEvenTernaryOperator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * 1. Write a java program that tells us that Input number is odd or even?
+ * HINT: use ternary operator (? :)
+ */
+
+public class Prog1OddEvenTernaryOperator {
+    public static void main(String[] args) {
+        //Scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number:");
+        int number = scanner.nextInt();
+        isItOddOrEvenNumber(number);
+        // closing the scanner object
+        scanner.close();
+    }
+
+    // Checking the number is even or odd
+    public static void isItOddOrEvenNumber(int number) {
+        //ternary operator is used
+        String evenOrOdd = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println("The " + number + " is " + evenOrOdd + " number");
+    }
+}
+
+
